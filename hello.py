e8634445de2145d359853dddb5becbdea83e49f8
sjj <2543070292@qq.com>
2022-11-21 12:32:27 +0800

    taile

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-print('你好')
-print('hello world')
-print(" ")
-print("test")
+## 
+n = int(input())
+sum = 0
+flag = 1
+for i in range(1,n+1):
+    sum += flag * ( 1 / i)
+    flag *= -1 
+print("{:.4f}".format(sum))    
